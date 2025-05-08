8ffc82d33ba186c11d929f079f545eecdbd000f7
Eyasu <eyasugetaneh2@gmail.com>
2025-05-08 14:36:09 +0300

    update signup

---
+++ app/src/main/java/com/example/elderlycare2/presentation/viewmodel/SignUpViewModel.kt
@@ -0,0 +1,30 @@
+package com.example.elderlycare2.presentation.viewmodel
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import com.example.elderlycare2.data.remote.api.request.SignUpRequest
+import com.example.elderlycare2.data.remote.response.AuthResponse
+import com.example.elderlycare2.data.repository.AuthRepository
+import com.example.elderlycare2.utils.ApiResult
+import dagger.hilt.android.lifecycle.HiltViewModel
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.StateFlow
+import kotlinx.coroutines.launch
+import javax.inject.Inject
+
+@HiltViewModel
+class SignUpViewModel @Inject constructor(
+    private val authRepository: AuthRepository
+) : ViewModel() {
+
+    private val _signUpState = MutableStateFlow<ApiResult<AuthResponse>>(ApiResult.Loading)
+    val signUpState: StateFlow<ApiResult<AuthResponse>> = _signUpState
+
+    fun signUp(name: String, email: String, password: String) {
+        viewModelScope.launch {
+            _signUpState.value = ApiResult.Loading
+            val result = authRepository.signUpUser(SignUpRequest(name, email, password))
+            _signUpState.value = result
+        }
+    }
+}
