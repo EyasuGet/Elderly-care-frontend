5fb36dfa58d03cfdd95baa4c6ea449d5d13546f8
fevenissayas <feven.issayas-ug@aau.edu.et>
2025-05-09 23:25:27 +0300

    edit profile updated

--- app/src/main/java/com/example/elderlycare2/data/repository/UserRepository.kt
+++ app/src/main/java/com/example/elderlycare2/data/repository/UserRepository.kt
@@ -1,26 +1,26 @@
 package com.example.elderlycare2.data.repository
 
-import com.example.elderlycare2.data.api.ApiService
-import com.example.elderlycare2.data.remote.request.LoginRequest
-import com.example.elderlycare2.data.remote.request.SignUpRequest
-import javax.inject.Inject
-
-class UserRepository @Inject constructor(
-    private val apiService: ApiService
-) {
-
-    suspend fun login(email: String, password: String) = runCatching {
-        val loginRequest = LoginRequest(email, password)
-        apiService.login(loginRequest)
-    }
-
-    suspend fun signupU(email: String, password: String, name: String) = runCatching {
-        val signupRequest = SignUpRequest(email, password, name)
-        apiService.signUpUser(signupRequest)
-    }
-
-    suspend fun signupN(email: String, password: String, name: String) = runCatching {
-        val signupRequest = SignUpRequest(email, password, name)
-        apiService.signUpNurse(signupRequest)
-    }
-}
+//import com.example.elderlycare2.data.api.ApiService
+//import com.example.elderlycare2.data.remote.request.LoginRequest
+//import com.example.elderlycare2.data.remote.request.SignUpRequest
+//import javax.inject.Inject
+//
+//class UserRepository @Inject constructor(
+//    private val apiService: ApiService
+//) {
+//
+//    suspend fun login(email: String, password: String) = runCatching {
+//        val loginRequest = LoginRequest(email, password)
+//        apiService.login(loginRequest)
+//    }
+//
+//    suspend fun signupU(email: String, password: String, name: String) = runCatching {
+//        val signupRequest = SignUpRequest(email, password, name)
+//        apiService.signUpUser(signupRequest)
+//    }
+//
+//    suspend fun signupN(email: String, password: String, name: String) = runCatching {
+//        val signupRequest = SignUpRequest(email, password, name)
+//        apiService.signUpNurse(signupRequest)
+//    }
+//}
